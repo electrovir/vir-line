de0b9d563923cc5ea00b5a81b713b8b3166373cb
electrovir <electrovir@users.noreply.github.com>
2024-07-02 23:24:17 +0000

    [patch] actually export VirLineWithState

--- packages/vir-line/src/index.ts
+++ packages/vir-line/src/index.ts
@@ -3,5 +3,6 @@ export * from './options';
 export * from './stage';
 export * from './state';
 export * from './vir-line';
+export * from './vir-line-with-state';
 
 export type {RemoveListenerCallback} from 'typed-event-target';
